c63166ddc5de8094e4e1b7bad427ab76f94ac437
sophchoe <sophchoe@gmail.com>
2024-07-11 12:02:20 -0700

    Create __init__.py

---
+++ quantum_transformers/__init__.py
@@ -0,0 +1,12 @@
+"""
+Quantum Transformers (QT) package.
+
+This package provides functionalities for building and training quantum transformers.
+"""
+
+# Import sub-packages
+from . import layers
+from . import models
+from . import utils
+
+__all__ = ['layers', 'models', 'utils']
