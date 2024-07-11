78ce4978cda2d926f3d0554c4d0af10ebc6fd20f
sophchoe <sophchoe@gmail.com>
2024-07-11 11:47:36 -0700

    Update __init__.py

--- quantum_neural_networks/src/__init__.py
+++ quantum_neural_networks/src/__init__.py
@@ -1,1 +1,14 @@
+"""
+Quantum Neural Networks (QNN) package.
 
+This package provides functionalities to create and train quantum neural networks.
+"""
+
+from .qnn_model import QuantumNeuralNetwork
+from .qnn_utils import prepare_data, evaluate_model
+
+__all__ = [
+    'QuantumNeuralNetwork',
+    'prepare_data',
+    'evaluate_model',
+]
