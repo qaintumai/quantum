d2e8f1818fb04427a4c7c5f2e6b3dea051ddeedc
sophchoe <sophchoe@gmail.com>
2024-07-11 09:08:34 -0700

    Update quantum_transformer.py

--- quantum_transformers/src/models/quantum_transformer.py
+++ quantum_transformers/src/models/quantum_transformer.py
@@ -1,1 +1,34 @@
+# Define the Transformer class
+import torch.nn as nn
+from models import InputEmbedding, EncoderBlock, DecoderBlock
 
+
+class Transformer(nn.Module):
+    def __init__(self, num_encoder_layers, num_decoder_layers, embed_len, num_heads, batch_size, vocab_size, dropout=0.1, device='cpu'):
+        super(Transformer, self).__init__()
+        self.embed_len = embed_len
+        self.device = device
+        self.embedding = InputEmbedding(
+            vocab_size, embed_len, dropout, device).to(device)
+        self.encoder_layers = nn.ModuleList([EncoderBlock(
+            embed_len, num_heads, batch_size, dropout).to(device) for _ in range(num_encoder_layers)])
+        self.decoder_layers = nn.ModuleList([DecoderBlock(
+            embed_len, num_heads, batch_size, dropout).to(device) for _ in range(num_decoder_layers)])
+        self.output_linear = nn.Linear(embed_len, vocab_size).to(device)
+
+    def forward(self, src, tgt):
+        src_embedded = self.embedding(src)
+        tgt_embedded = self.embedding(tgt)
+
+        # Encoder forward pass
+        encoder_output = src_embedded
+        for layer in self.encoder_layers:
+            encoder_output = layer(
+                encoder_output, encoder_output, encoder_output)
+
+        # Decoder forward pass
+        decoder_output = tgt_embedded
+        for layer in self.decoder_layers:
+            decoder_output = layer(decoder_output, encoder_output)
+
+        return self.output_linear(decoder_output)
