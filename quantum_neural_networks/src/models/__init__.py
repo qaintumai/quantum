6f0d2215a16daf2930f1033ee4470433cb711ab1
sophchoe <sophchoe@gmail.com>
2024-07-11 11:51:48 -0700

    Update __init__.py

--- quantum_neural_networks/src/models/__init__.py
+++ quantum_neural_networks/src/models/__init__.py
@@ -1,1 +1,12 @@
+"""
+Models for Quantum Neural Networks (QNN).
+
+This package includes the model definitions for QNN.
+"""
+
+from .qnn_model import QuantumNeuralNetwork
+
+__all__ = [
+    'QuantumNeuralNetwork',
+]
 
