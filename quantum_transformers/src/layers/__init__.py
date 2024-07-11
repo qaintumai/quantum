b4b986e8ad2c6e01d2a83cbe54123a7e7d0c90d7
sophchoe <sophchoe@gmail.com>
2024-07-11 12:20:50 -0700

    Update __init__.py

--- quantum_transformers/src/layers/__init__.py
+++ quantum_transformers/src/layers/__init__.py
@@ -5,11 +5,15 @@ This package includes various layer implementations for QTs.
 """
 
 from .input_embedding import InputEmbedding
-from .multi_head_attention import MultiHeadAttention
+from .multi_headed_attention import MultiHeadedAttention
 from .quantum_data_encoding import QuantumDataEncoding
+from .quantum_feed_forward import QuantumFeedForward
+from .scale_dot_product import ScaleDotProduct
 
 __all__ = [
     'InputEmbedding',
-    'MultiHeadAttention',
-    'QuantumDataEncoding'
+    'MultiHeadedAttention',
+    'QuantumDataEncoding',
+    'QuantumFeedForward',
+    'ScaleDotProduct'
 ]
