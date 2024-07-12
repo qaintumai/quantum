0a8cbba2bda42c67aeebef4061c4a9b9af5dcf21
sophchoe <sophchoe@gmail.com>
2024-07-11 19:52:54 -0700

    Update __init__.py

--- quantum_neural_networks/src/utils/__init__.py
+++ quantum_neural_networks/src/utils/__init__.py
@@ -1,13 +1,1 @@
-"""
-Utility functions for Quantum Neural Networks (QNN).
-
-This package includes various utility functions for data preparation and model evaluation.
-"""
-
-from .qnn_utils import prepare_data, evaluate_model
-
-__all__ = [
-    'prepare_data',
-    'evaluate_model',
-]
-
+from .qnn_utils import preprocess_data, train_model, evaluate_model, visualize_training
