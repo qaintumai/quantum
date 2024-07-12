0915c19fbdf0b4c0fb249e71653156f426a730f9
sophchoe <sophchoe@gmail.com>
2024-07-11 17:27:34 -0700

    Update qnn_model.py

--- quantum_neural_networks/src/models/qnn_model.py
+++ quantum_neural_networks/src/models/qnn_model.py
@@ -9,36 +9,33 @@ Quantum neural network model implementation using Pennylane and PyTorch.
 import torch
 import numpy as np
 import pennylane as qml
+from .qnn_layer import data_encoding, qnn_layer, init_weights
 
-# Think through the output
-num_modes = 8
+num_wires = 8
 num_basis = 2
 
 # select a device
-dev = qml.device("strawberryfields.fock", wires=num_modes, cutoff_dim=num_basis)
-
-# Initialize the Quantum Neural Network
-qnn = QuantumNeuralNetwork(num_wires=num_modes)
+dev = qml.device("strawberryfields.fock", wires=num_wires, cutoff_dim=num_basis)
 
 @qml.qnode(dev, interface="torch")
 def quantum_nn(inputs, var):
-    qnn.data_encoding(inputs)
+    # convert classical inputs into quantum states
+    data_encoding(inputs)
 
     # iterative quantum layers
     for v in var:
-        qnn.qnn_layer(v)
+        qnn_layer(v)
 
     return qml.expval(qml.X(0))
 
+def get_model(num_modes, num_layers):
+    weights = init_weights(num_layers, num_modes)
+    shape_tup = weights.shape
+    weight_shapes = {'var': shape_tup}
+    qlayer = qml.qnn.TorchLayer(quantum_nn, weight_shapes)
+    model = torch.nn.Sequential(qlayer)
+    return model
+
 num_layers = 2
 
-# initialize weights for quantum layers
-weights = qnn.init_weights(num_layers, num_modes)
-
-# convert the quantum layer to a PyTorch layer
-shape_tup = weights.shape
-weight_shapes = {'var': shape_tup}
-
-qlayer = qml.qnn.TorchLayer(quantum_nn, weight_shapes)
-layers = [qlayer]
-model = torch.nn.Sequential(*layers)
+model = get_model(num_modes, num_layers)
