1c2a5b9167dcec60e38979ef77c15504f345261b
sophchoe <sophchoe@gmail.com>
2024-07-11 18:07:56 -0700

    Update qnn_layer.py

--- quantum_neural_networks/src/models/qnn_layer.py
+++ quantum_neural_networks/src/models/qnn_layer.py
@@ -9,7 +9,8 @@ quantum neural network layer developed in Pennylane.
 import numpy as np
 import pennylane as qml
 
-def data_encoding(x, num_wires=8):
+def data_encoding(x):
+    num_wires=8
     num_features = len(x)
 
     for i in range(0, min(num_features, num_wires * 2), 2):
@@ -45,7 +46,8 @@ def data_encoding(x, num_wires=8):
         if idx < num_features:
             qml.Rotation(x[idx], wires=i)
 
-def qnn_layer(v, num_wires=8):
+def qnn_layer(v):
+    num_wires=8
     num_params = len(v)
 
     for i in range(num_wires - 1):
@@ -87,14 +89,14 @@ def qnn_layer(v, num_wires=8):
         if idx < num_params:
             qml.Kerr(v[idx], wires=i)
 
-def init_weights(layers, modes, active_sd=0.0001, passive_sd=0.1):
-    M = (modes-1)*2 + modes
+def init_weights(layers, num_wires, active_sd=0.0001, passive_sd=0.1):
+    M = (modes-1)*2 + num_wires
 
     int1_weights = np.random.normal(size=[layers, M], scale=passive_sd)
-    s_weights = np.random.normal(size=[layers, modes], scale=active_sd)
+    s_weights = np.random.normal(size=[layers, num_wires], scale=active_sd)
     int2_weights = np.random.normal(size=[layers, M], scale=passive_sd)
-    dr_weights = np.random.normal(size=[layers, modes], scale=active_sd)
-    k_weights = np.random.normal(size=[layers, modes], scale=active_sd)
+    dr_weights = np.random.normal(size=[layers, num_wires], scale=active_sd)
+    k_weights = np.random.normal(size=[layers, num_wires], scale=active_sd)
 
     weights = np.concatenate([int1_weights, s_weights, int2_weights, dr_weights, k_weights], axis=1)
 
