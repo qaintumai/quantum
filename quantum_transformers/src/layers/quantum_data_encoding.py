0b4a7946525b024021a7d75dbf25e75697c22212
sophchoe <sophchoe@gmail.com>
2024-07-11 20:26:58 -0700

    Update quantum_data_encoding.py

--- quantum_transformers/src/layers/quantum_data_encoding.py
+++ quantum_transformers/src/layers/quantum_data_encoding.py
@@ -2,18 +2,14 @@
 Quantum Data Encoding Layer for Quantum Feedforward using Quantum Neural Networks (QNN).
 """
 
-from ..src/models import data_encoding
+from quantum_neural_networks.src.models.qnn_layer import data_encoding
 
-class QuantumDataEncoding:
-    def __init__(self, num_wires=8):
-        self.qnn = QuantumNeuralNetwork(num_wires=num_wires)
+def quantum_data_encoding(x):
+    """
+    Encodes the input data using the data_encoding function from qnn_layer.py.
 
-    def encode(self, x):
-        """
-        Encodes the input data using the QuantumNeuralNetwork's data_encoding method.
-
-        Parameters:
-        x (array-like): Input data to be encoded.
-        """
-        self.qnn.data_encoding(x)
-        # Additional functionality or return value can be added here
+    Parameters:
+    x (array-like): Input data to be encoded.
+    """
+    data_encoding(x)
+    # Additional functionality or return value can be added here
