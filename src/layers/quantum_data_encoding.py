29c805332090d73f5fa145aa5d58780c7b015f91
sophchoe <sophchoe@gmail.com>
2024-07-17 14:19:21 -0700

    Create quantum_data_encoding.py

---
+++ src/layers/quantum_data_encoding.py
@@ -0,0 +1,57 @@
+import pennylane as qml
+# Define the QuantumDataEncoding class
+
+
+class QuantumDataEncoding:
+    def __init__(self, num_wires):
+        self.num_wires = num_wires
+
+    def encode(self, x):
+        num_features = len(x)
+
+        # Squeezing gates
+        for i in range(0, min(num_features, self.num_wires * 2), 2):
+            qml.Squeezing(x[i], x[i + 1], wires=i // 2)
+
+        # Beamsplitter gates
+        for i in range(self.num_wires - 1):
+            idx = self.num_wires * 2 + i * 2
+            if idx + 1 < num_features:
+                qml.Beamsplitter(x[idx], x[idx + 1], wires=[i %
+                                 self.num_wires, (i + 1) % self.num_wires])
+
+        # Rotation gates
+        for i in range(self.num_wires):
+            idx = self.num_wires * 2 + (self.num_wires - 1) * 2 + i
+            if idx < num_features:
+                qml.Rotation(x[idx], wires=i)
+
+        # Displacement gates
+        for i in range(self.num_wires):
+            idx = self.num_wires * 2 + \
+                (self.num_wires - 1) * 2 + self.num_wires + i * 2
+            if idx + 1 < num_features:
+                qml.Displacement(x[idx], x[idx + 1], wires=i)
+
+        # Kerr gates
+        for i in range(self.num_wires):
+            idx = self.num_wires * 2 + \
+                (self.num_wires - 1) * 2 + \
+                self.num_wires + self.num_wires * 2 + i
+            if idx < num_features:
+                qml.Kerr(x[idx], wires=i)
+
+        # Squeezing gates (second set)
+        for i in range(0, min(num_features - (self.num_wires * 2 + (self.num_wires - 1) * 2 + self.num_wires + self.num_wires * 2 + self.num_wires), self.num_wires * 2), 2):
+            idx = self.num_wires * 2 + \
+                (self.num_wires - 1) * 2 + self.num_wires + \
+                self.num_wires * 2 + self.num_wires + i
+            if idx + 1 < num_features:
+                qml.Squeezing(x[idx], x[idx + 1], wires=i // 2)
+
+        # Rotation gates (second set)
+        for i in range(self.num_wires):
+            idx = self.num_wires * 2 + (self.num_wires - 1) * 2 + self.num_wires + \
+                self.num_wires * 2 + self.num_wires + self.num_wires * 2 + i
+            if idx < num_features:
+                qml.Rotation(x[idx], wires=i)
