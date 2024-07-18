b54e4724a72378ce01f0f4917cc13076470fdfd1
sophchoe <sophchoe@gmail.com>
2024-07-17 19:42:42 -0700

    Update quantum_neural_network.py

--- src/models/quantum_neural_network.py
+++ src/models/quantum_neural_network.py
@@ -1,14 +1,15 @@
 class QuantumNeuralNetworkModel:
-    def __init__(self, num_layers, num_wires):
+    def __init__(self, num_layers, num_wires, quantum_nn):
         self.num_layers = num_layers
         self.num_wires = num_wires
+        self.quantum_nn = quantum_nn
         self.model = self._build_model()
 
     def _build_model(self):
         weights = WeightInitializer.init_weights(self.num_layers, self.num_wires)
         shape_tup = weights.shape
         weight_shapes = {'var': shape_tup}
-        qlayer = qml.qnn.TorchLayer(quantum_nn, weight_shapes)
+        qlayer = qml.qnn.TorchLayer(self.quantum_nn, weight_shapes)
         model = Sequential(qlayer)
         return model
 
