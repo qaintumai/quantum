2685fe29e000baf0c82c28f54d1acd5c6f05bdc9
sophchoe <sophchoe@gmail.com>
2024-07-17 19:34:19 -0700

    Create qnn_multi_output.py

---
+++ src/layers/qnn_multi_output.py
@@ -0,0 +1,21 @@
+import pennylane as qml
+
+# Define the number of wires and basis states
+num_wires = 8
+num_basis = 2
+
+# Select a device
+dev = qml.device("strawberryfields.fock", wires=num_wires, cutoff_dim=num_basis)
+
+@qml.qnode(dev, interface="torch")
+def quantum_circuit_multi_output(inputs, var):
+    encoder = QuantumDataEncoder(num_wires)
+    encoder.encode(inputs)
+
+    # Iterative quantum layers
+    q_layer = QuantumNeuralNetworkLayer(num_wires)
+    for v in var:
+        q_layer.apply(v)
+
+    # Return the probabilities for all wires
+    return [qml.expval(qml.X(wire)) for wire in range(num_wires)]
