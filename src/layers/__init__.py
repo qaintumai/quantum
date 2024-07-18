5481ec7ac1e1d8001bdde4d63c84b90e1d84a0e6
sophchoe <sophchoe@gmail.com>
2024-07-17 19:38:30 -0700

    Update __init__.py

--- src/layers/__init__.py
+++ src/layers/__init__.py
@@ -1,7 +1,8 @@
 from .input_embedding import InputEmbedding
 from .multi_headed_attention import MultiHeadedAttention
-from .quantum_circuit_multi_output import QuantumCircuitMultiOutput
-from .quantum_circuit_single_output import QuantumCircuitSingleOutput
+from .qnn_multi_output import qnn_multi_output
+from .qnn_probabilities import qnn_probabilities
+from .qnn_single_output import nn_single_output
 from .quantum_data_encoding import QuantumDataEncoder
 from .quantum_feed_forward import QuantumFeedForward
 from .quantum_layer import QuantumLayer
@@ -11,8 +12,9 @@ from .weight_initializer import WeightInitializer
 __all__ = [
     "InputEmbedding",
     "MultiHeadedAttention",
-    "QuantumCircuitMultiOutput",
-    "QuantumCircuitSingleOutput",
+    "qnn_multi_output",
+    "qnn_probabilities",
+    "qnn_single_output",
     "QuantumDataEncoder",
     "QuantumFeedForward",
     "QuantumLayer",
