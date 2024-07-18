ff05b04b208c6d5547e76a187820120456cfda42
sophchoe <sophchoe@gmail.com>
2024-07-17 19:23:47 -0700

    Update quantum_feed_forward.py

--- src/layers/quantum_feed_forward.py
+++ src/layers/quantum_feed_forward.py
@@ -5,55 +5,9 @@ Essentially, this is a quantum neural network (QNN).
 This file is a specific QNN as a quantum version of the feed forward block of a transformer.
 """
 
-import torch.nn as nn
-import pennylane as qml
-
-from models import DataEncoding
-from models import QuantumLayer
-from models import WeightInitializer
-
-# Think through the output
-num_modes = 6
-num_basis = 2
-
-# Select a device
-dev = qml.device("strawberryfields.fock",
-                 wires=num_modes, cutoff_dim=num_basis)
-
-
-@qml.qnode(dev, interface="torch")
-def quantum_nn(inputs, var):
-    num_wires = 6
-    encoder = DataEncoding(num_wires)
-    encoder.encode(inputs)
-
-    # Iterative quantum layers
-    q_layer = QuantumLayer(num_wires)
-    for v in var:
-        q_layer.apply_layer(v)
-
-    # Return the probabilities
-    return qml.probs(wires=[0, 1, 2, 3, 4, 5])
-
-
-num_layers = 2
-
-# Initialize weights for quantum layers
-weights = WeightInitializer.init_weights(num_layers, num_modes)
-
-# Convert the quantum layer to a Torch layer
-shape_tup = weights.shape
-weight_shapes = {'var': shape_tup}
-
-qlayer = qml.qnn.TorchLayer(quantum_nn, weight_shapes)
-layers = [qlayer]
-
-# Define the FeedForwardBlock class
-
-
-class FeedForwardBlock(nn.Module):
+class QuantumFeedForward(nn.Module):
     def __init__(self, embed_len, dropout=0.1):
-        super(FeedForwardBlock, self).__init__()
+        super(QuantumFeedForward, self).__init__()
         self.feed_forward = nn.Sequential(*layers)
         self.dropout_layer = nn.Dropout(p=dropout)
         self.layer_norm = nn.LayerNorm(embed_len)
@@ -66,7 +20,7 @@ class FeedForwardBlock(nn.Module):
 
 # Example usage
 embed_len = 64  # example value
-model = FeedForwardBlock(embed_len)
+model = QuantumFeedForward(embed_len)
 
 # Calculate the number of parameters
 
