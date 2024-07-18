b62a02fa9e5cf830913ea17386484ae903d00221
sophchoe <sophchoe@gmail.com>
2024-07-17 20:09:16 -0700

    Delete quantum_neural_networks directory

--- quantum_neural_networks/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Tests for the Quantum Neural Networks (QNN) package.
-
-This package includes unit tests for various components of the QNN.
-"""
-
-# Import necessary modules for testing
-import unittest
-
